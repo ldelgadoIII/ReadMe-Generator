304c6f564dc595e6641c80dafd7bab9cf6b4c38b
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-02-03 18:04:57 -0600

    added questions to an array

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,14 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  "What is the title of your project?",
+  "What is your project about?",
+  "How do you install it?",
+  "How is this project used?",
+  "What are the contribution guidelines?",
+  "What are the test instructions?",
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -13,3 +20,15 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      question: "What is your name?",
+      name: "username",
+    },
+  ])
+  .then((reponse) => {
+    console.log(reponse.username);
+  });
